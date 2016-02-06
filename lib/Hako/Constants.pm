1da7b9d57d7590598403acefb220564a61c86a4d
Fumihiro Itoh <fmhrit@gmail.com>
2016-02-06 16:50:40 +0900

    ゲームに関する設定をyamlに分離した

---
+++ lib/Hako/Constants.pm
@@ -0,0 +1,21 @@
+package Hako::Constants;
+use strict;
+use warnings;
+use File::Spec;
+use File::Basename;
+use YAML;
+
+sub import {
+    my $config = YAML::LoadFile(File::Spec->catfile(dirname(__FILE__), "../../constants.yaml"));
+
+    for my $key (keys %{$config}) {
+        no strict 'refs';
+        no warnings 'redefine';
+        my $conf_name = uc($key);
+        *{__PACKAGE__."::".$conf_name} = sub() { $config->{$key} };
+    }
+}
+
+sub DIR_MODE { return 0755; }
+
+1;
